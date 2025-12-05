9388ae08c660412d1e7ca363d0d428068fd97c69
MGSimard <mgsimard.dev@gmail.com>
2025-12-05 11:57:35 -0500

    Fonts

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,24 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { motion } from "motion/react";
+import indexCss from "./index.css?url";
 
-export const Route = createFileRoute("/")({ component: App });
+export const Route = createFileRoute("/")({
+  head: () => ({
+    links: [
+      {
+        rel: "stylesheet",
+        href: indexCss,
+      },
+    ],
+  }),
+  component: App,
+});
 
 function App() {
   return (
     <div>
       <h1>MGSIMARD</h1>
+      <motion.button className="test" initial={{ scale: 0 }} animate={{ scale: 1 }} />
     </div>
   );
 }
