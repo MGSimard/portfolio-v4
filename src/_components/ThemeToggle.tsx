4f81dc55b46fefdebd9487ab16b61309b492cc00
MGSimard <mgsimard.dev@gmail.com>
2026-01-05 11:26:31 -0500

    Adjust svg

--- src/_components/ThemeToggle.tsx
+++ src/_components/ThemeToggle.tsx
@@ -20,8 +20,26 @@ export function ThemeToggle() {
       size="icon"
       className="fixed bottom-4 right-4 text-primary/50 hover:text-primary focus-visible:text-primary outline-none focus-visible:ring-1"
       onClick={() => setTheme(activeTheme === "light" ? "dark" : "light")}>
-      {activeTheme === "light" ? <IconBoltSlash className="size-5" /> : <IconBolt className="size-5" />}
+      {activeTheme === "light" ? <IconBolt className="size-5" /> : <IconBoltSlash className="size-5" />}
     </Button>
+  );
+}
+
+function IconBolt(props: React.ComponentProps<"svg">) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="currentColor"
+      viewBox="0 0 24 24"
+      strokeWidth="1.5"
+      stroke="currentColor"
+      {...props}>
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="m3.75 13.5 10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75Z"
+      />
+    </svg>
   );
 }
 
@@ -38,24 +56,6 @@ function IconBoltSlash(props: React.ComponentProps<"svg">) {
         strokeLinecap="round"
         strokeLinejoin="round"
         d="M11.412 15.655 9.75 21.75l3.745-4.012M9.257 13.5H3.75l2.659-2.849m2.048-2.194L14.25 2.25 12 10.5h8.25l-4.707 5.043M8.457 8.457 3 3m5.457 5.457 7.086 7.086m0 0L21 21"
-      />
-    </svg>
-  );
-}
-
-function IconBolt(props: React.ComponentProps<"svg">) {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      strokeWidth="1.5"
-      stroke="currentColor"
-      {...props}>
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="m3.75 13.5 10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75Z"
       />
     </svg>
   );
