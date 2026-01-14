e8c952384b777f97ad7bb6074dbfa90fd6661ebe
MGSimard <mgsimard.dev@gmail.com>
2026-01-14 09:23:20 -0500

    Fix up canonical

--- src/routes/_with-header/gamedev.tsx
+++ src/routes/_with-header/gamedev.tsx
@@ -1,8 +1,12 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { ProjectSection } from "@/_components/ProjectSection";
 import { projectsGamedev } from "@/_data/projects-gamedev";
+import { SITE_URL } from "@/_lib/consts";
 
 export const Route = createFileRoute("/_with-header/gamedev")({
+  head: () => ({
+    links: [{ rel: "canonical", href: `${SITE_URL}/gamedev` }],
+  }),
   component: RouteComponent,
 });
 
--- src/routes/_with-header/webdev.tsx
+++ src/routes/_with-header/webdev.tsx
@@ -1,8 +1,12 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { ProjectSection } from "@/_components/ProjectSection";
 import { projectsWebdev } from "@/_data/projects-webdev";
+import { SITE_URL } from "@/_lib/consts";
 
 export const Route = createFileRoute("/_with-header/webdev")({
+  head: () => ({
+    links: [{ rel: "canonical", href: `${SITE_URL}/webdev` }],
+  }),
   component: RouteComponent,
 });
 
